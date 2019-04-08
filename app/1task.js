ca9f5d075d8334860f1c2564b788b94ab8c842ad
vdmytriukhin <vdmytriukhin@foundationmedicine.com>
2019-04-08 16:07:14 +0300

    added 1 task

---
+++ app/1task.js
@@ -0,0 +1,16 @@
+/**
+ * Implemen function that takes two numbers and returns the sum of this two numbers.
+ * Implement unit tests for this function - test should cover all requirements below.
+ *  - If any of arguments is not a number - return message "Incorrect arguments type" and value of argument.
+ *  - If argument a String that can be casting to a number ("123") it should be caster to number.
+ *  - If there is only 1 argument instead of 2 - return message "Second argument is needed.
+ *  - If there are more than 2 arguments - return message "There are more than 2 arguments"
+ * @param a
+ * @param b
+ * @returns {undefined}
+ */
+let addition = function(a, b) {
+    return undefined;
+};
+
+module.exports = addition;
---
+++ test/1task.js
@@ -0,0 +1,8 @@
+let app = require('C:\\Users\\Volodymyr_Dmytriukhi\\WebstormProjects\\untitled\\app\\1task.js');
+var assert = require('assert');
+
+describe('', () => {
+    it('should ', function () {
+        assert.equal(app(2, 7), 9);
+    });
+});
